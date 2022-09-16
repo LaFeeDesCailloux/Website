8b0451c03f005d9db40091000c53f17ba5ed8778
Mathis <contact@minarox.fr>
2022-09-17 01:45:07 +0200

    Create contact component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -178,22 +178,12 @@
     </section>
   </article>
 
-  <article id="contact">
-    <section class="content">
-      <h2>Me contacter</h2>
-      <p>
-        Vous avez envie d'acheter l'une de mes créations ou besoins de
-        renseignements ?<br />
-        N'hésitez pas à me contacter par téléphone, mail ou avec le formulaire
-        disponible sur la page de contact.
-      </p>
-      <router-link to="/">Contacter</router-link>
-    </section>
-  </article>
+  <Contact />
 </template>
 
 <script>
 import "@splidejs/vue-splide/css";
+import Contact from "@/components/Contact";
 
 export default {
   name: "homeView",
@@ -227,6 +217,12 @@ export default {
         gap: "4rem",
       },
     };
+  },
+  components: {
+    Splide,
+    SplideTrack,
+    SplideSlide,
+    Contact,
   },
 };
 </script>
@@ -509,45 +505,6 @@ export default {
           bottom: -1rem;
           right: -0.2rem;
         }
-      }
-    }
-  }
-}
-
-#contact {
-  background-color: rgba(0, 0, 0, 0.2);
-
-  section {
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    gap: 2rem;
-
-    h2 {
-      font-size: 2em;
-      text-align: center;
-    }
-
-    p {
-      text-align: center;
-    }
-
-    a {
-      display: block;
-      text-align: center;
-      padding: 0.8rem 1.8rem;
-      border: 2px solid black;
-      background-color: white;
-      border-radius: 6px;
-      font-size: 1em;
-      color: black;
-      text-decoration: none;
-      font-weight: bold;
-      transform: scale(1);
-      transition: transform 250ms;
-
-      &:hover {
-        transform: scale(1.06);
       }
     }
   }
