99af083a85396ec61f6e17b89dc0ba6c89de0ec5
Minarox <contact@minarox.fr>
2022-09-19 12:32:23 +0200

    Split content into multiple components

---
+++ src/components/realisations/banniere.vue
@@ -0,0 +1,70 @@
+<template>
+  <article>
+    <h1>{{ titre }}</h1>
+    <img
+      :src="require('@/assets/realisations/' + image)"
+      :alt="titre"
+      loading="lazy"
+    />
+    <i />
+  </article>
+</template>
+
+<script>
+export default {
+  name: "banniereComponent",
+  props: ["titre", "image"],
+};
+</script>
+
+<style scoped lang="scss">
+article {
+  z-index: 0;
+  background-color: #070707;
+  position: relative;
+
+  h1 {
+    z-index: -1;
+    text-align: center;
+    font-size: 8em;
+    color: white;
+    position: absolute;
+    top: 3rem;
+    left: 0;
+    right: 0;
+  }
+
+  img {
+    display: block;
+    padding-top: 8.4rem;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  i {
+    position: absolute;
+    top: 95%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgb(255, 255, 255);
+    background: -moz-linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    background: -webkit-linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#ffffff",endColorstr="#ffffff",GradientType=1);
+  }
+}
+</style>
---
+++ src/components/realisations/specificites.vue
@@ -0,0 +1,55 @@
+<template>
+  <article>
+    <section class="content">
+      <h2>Spécificités</h2>
+      <div ref="wrapper">
+        <slot />
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "specificitesComponent",
+  mounted() {
+    this.$refs.wrapper.style.gridTemplateColumns =
+      "repeat(" + this.$refs.wrapper.children.length + ", 1fr)";
+  },
+};
+</script>
+
+<style scoped lang="scss">
+article {
+  background-color: rgba(0, 0, 0, 0.1);
+
+  h2 {
+    font-size: 2em;
+    text-align: center;
+    margin-bottom: 3rem;
+  }
+
+  section > div {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 2rem;
+
+    :slotted(> div) {
+      padding: 1.5rem;
+      background-color: white;
+      border-radius: 6px;
+      display: flex;
+      flex-flow: column nowrap;
+      align-items: center;
+      gap: 1rem;
+      text-align: center;
+      box-shadow: rgba(0, 0, 0, 0) 0 0 0 0, rgba(0, 0, 0, 0) 0 0 0 0,
+        rgba(0, 0, 0, 0.25) 0 25px 50px -12px;
+
+      svg {
+        font-size: 1.7em;
+      }
+    }
+  }
+}
+</style>
