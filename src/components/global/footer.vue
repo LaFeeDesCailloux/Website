c1a06c0d9af92a22db79ac50484e3fee78af0906
Minarox <contact@minarox.fr>
2022-11-13 00:12:43 +0100

    Update style

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -1,42 +1,40 @@
 <template>
   <footer>
-    <section>
-      <p>
-        © Tous droits réservés |
-        <router-link :to="{ name: 'home' }">LaFeeDesCailloux.fr</router-link>
-      </p>
-      <p>
-        Site Web réalisé par <a href="https://github.com/Minarox">Mathis</a>.
-      </p>
-    </section>
+    <p>
+      © {{ current_year }} - Tous droits réservés &nbsp;|&nbsp;
+      <router-link :to="{ name: 'home' }">LaFeeDesCailloux.fr</router-link>
+    </p>
+    <p>Site Web réalisé par <a href="https://github.com/Minarox">Mathis</a>.</p>
   </footer>
 </template>
 
 <script>
 export default {
   name: "footerComponent",
+  computed: {
+    current_year() {
+      return new Date().getFullYear();
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 footer {
-  padding: 1rem;
-  background-color: var(--secondary-bg-color);
+  padding: 1.5rem 2rem;
   display: flex;
-  justify-content: center;
+  flex-flow: row wrap;
+  width: min(100%, var(--content-max-width));
+  margin-inline: auto;
+  align-items: center;
+  justify-content: space-between;
   font-size: 0.8em;
+  opacity: 0.9;
+  gap: 0.5rem 2rem;
 
-  section {
-    width: min(100%, var(--max-width-global));
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 1rem;
-
-    a {
-      text-decoration: none;
-      color: black;
-    }
+  a {
+    text-decoration: none;
+    color: black;
   }
 }
 </style>
