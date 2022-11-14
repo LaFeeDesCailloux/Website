be272495e067e68e85705a6e90609fc6ecc91f13
Minarox <contact@minarox.fr>
2022-11-14 15:30:11 +0100

    Rewrite banner

--- src/components/home/banniere.vue
+++ src/components/home/banniere.vue
@@ -1,7 +1,15 @@
 <template>
   <article>
+    <img id="stones" src="@/assets/img/banner.webp" alt="Bannière" />
+    <img id="fee" src="@/assets/svg/fée.svg" alt="Fée" />
+    <img id="separator" src="@/assets/svg/main-banner.svg" alt="Séparateur" />
     <section class="content">
-      <h1>Laissez vous porter par la puissance des pierres.</h1>
+      <h1>
+        Laissez vous porter<br />
+        par la puissance des pierres.
+      </h1>
+      <a @click.prevent="scrollToRealisation">Découvrir mon travail</a>
+      <img src="@/assets/svg/cristaux.svg" alt="Cristaux" />
     </section>
   </article>
 </template>
@@ -9,24 +17,92 @@
 <script>
 export default {
   name: "banniereComponent",
+  methods: {
+    scrollToRealisation() {
+      const element = document.getElementById("mes-realisations");
+      const y = element.getBoundingClientRect().top + window.pageYOffset - 100;
+      window.scrollTo({ top: y, behavior: "smooth" });
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 article {
-  height: 400px;
-  background-image: url("@/assets/img/banner.webp");
-  background-size: cover;
-  background-position: right;
+  position: relative;
+  height: 660px;
   background-color: #eeeff1;
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
 
-  h1 {
-    max-width: 900px;
-    font-size: 4em;
-    text-shadow: 0 0 20px white;
+  #stones {
+    position: absolute;
+    right: 0;
+    height: inherit;
+    transform: scaleX(-1);
+  }
+
+  #fee {
+    position: absolute;
+    right: 0;
+    left: 4rem;
+    top: 90px;
+    margin-inline: auto;
+    height: 300px;
+    transform: rotate(14deg);
+    filter: drop-shadow(0 0 12px #eeeff1);
+  }
+
+  #separator {
+    position: absolute;
+    top: 180px;
+    width: 100%;
+    filter: drop-shadow(0 -20px 10px rgb(0, 0, 0, 0.1));
+  }
+
+  .content {
+    position: absolute;
+    top: 180px;
+    left: 0;
+    right: 0;
+
+    h1 {
+      z-index: 1;
+      position: relative;
+      font-size: 3.8em;
+      text-align: left;
+      margin-bottom: 1.5rem;
+    }
+
+    a {
+      z-index: 1;
+      position: relative;
+      font-size: 1.4em;
+      display: inline-block;
+      text-align: center;
+      padding: 0.6rem 4rem;
+      border: 3px solid var(--special-text-color);
+      border-radius: var(--border-radius);
+      color: var(--special-text-color);
+      background-color: var(--global-bg-color);
+      text-decoration: none;
+      font-weight: bold;
+      transform: scale(1.005) translateX(0);
+      transition: transform 400ms;
+      cursor: pointer;
+
+      &:hover {
+        transform: scale(1.04) translateX(6px);
+      }
+    }
+
+    img {
+      opacity: 0.3;
+      z-index: 0;
+      position: absolute;
+      bottom: 0;
+      left: 192px;
+      height: 300px;
+      transform: rotate(20deg);
+    }
   }
 }
 </style>
