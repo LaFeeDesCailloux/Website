006132672995ea71fab828351b382556201aa2d7
Minarox <contact@minarox.fr>
2023-01-08 15:55:39 +0100

    Add legal pages in footer

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -4,9 +4,16 @@
       © {{ current_year }} - Tous droits réservés &nbsp;|&nbsp;
       <router-link :to="{ name: 'home' }">LaFeeDesCailloux.fr</router-link>
     </p>
-    <p>
-      Site Web réalisé par <a href="https://github.com/Minarox">Mathis SM</a>.
-    </p>
+    <section>
+      <router-link :to="{ name: 'plan-du-site' }">Plan du site</router-link>
+      <router-link :to="{ name: 'mentions-legales' }">
+        Mentions légales
+      </router-link>
+      <router-link :to="{ name: 'cgu' }">CGU</router-link>
+      <router-link :to="{ name: 'politique-de-confidentialite' }">
+        Politique de confidentialité
+      </router-link>
+    </section>
   </footer>
 </template>
 
@@ -25,7 +32,7 @@ export default {
 footer {
   opacity: 0.8;
   width: min(100%, var(--content-max-width));
-  padding: 1.5rem 2rem;
+  padding: 1.2rem 2rem;
   margin-inline: auto;
   display: flex;
   flex-direction: column;
@@ -35,13 +42,24 @@ footer {
   font-size: 0.8em;
   text-align: center;
 
-  @media (min-width: 580px) {
+  @media (min-width: 900px) {
     flex-direction: row;
   }
 
   a {
     text-decoration: none;
     color: black;
+  }
+
+  section {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      padding: 0.3rem 0.9rem;
+    }
   }
 }
 </style>
