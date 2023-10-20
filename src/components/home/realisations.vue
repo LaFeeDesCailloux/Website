59eb7163d2667c90127c19241bfbb315215c7b53
Minarox <contact@minarox.fr>
2023-10-20 13:12:46 +0200

    feat(price): Changement prix chemins de vie

--- src/components/home/realisations.vue
+++ src/components/home/realisations.vue
@@ -11,8 +11,8 @@
         <div>
           <h3>Chemins de vie</h3>
           <p>
-            A partir de 40€<br />
-            jusqu'à 64€
+            A partir de 45€<br />
+            jusqu'à 60€
           </p>
         </div>
       </router-link>
